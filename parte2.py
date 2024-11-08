79e34ffca98d89d9fe6f4274d7b48aabbc193bd1
er tiruzz <tirino.christian@iisgalvanimi.edu.it>
2024-11-08 12:00:08 +0000

    Commit 5

--- parte2.py
+++ parte2.py
@@ -27,8 +27,18 @@ def addfilm(data):
     return mycursor.rowcount
 
 def deletefilm(Id):
+    mycursor = mydb.cursor()
     query = "DELETE FROM film WHERE Id = %s"
     mycursor.execute(query, (Id,))
+    mydb.commit()
+    rowcount = mycursor.rowcount
+    mycursor.close()
+    return rowcount
+
+def updatefilm(Id, data):
+    query = "UPDATE film SET Titolo = %s, Regista = %s, Genere = %s, Anno_uscita = %s, Cast_principale = %s WHERE id = %s"
+    values = (data['Titolo'], data['Regista'], data['Genere'], data['Anno_uscita'], data["Cast_principale"], id)
+    mycursor.execute(query, values)
     mydb.commit()
     return mycursor.rowcount
 
@@ -53,13 +63,30 @@ def add():
     else:
         return jsonify({'message': 'Errore durante l inserimento'}), 500
 
-@app.route("/delete/<Id>", methods=["DELETE"])
+@app.route("/delete/<int:Id>", methods=["DELETE"])
 def delete(Id):
     rows_deleted = deletefilm(Id)
     if rows_deleted == 1:
         return jsonify({'message': 'Film eliminato'}), 200
     else:
         return jsonify({'message': 'ID non trovato'}), 404
+
+@app.route("/update/<Id>", methods=["PUT"])
+def update(Id):
+    print(request.json)
+    print("ciao")
+    data = request.json
+    
+    required_fields = ['Titolo', 'Regista', 'Genere', 'Anno_uscita', 'Cast_principale']
+    if not all(field in data for field in required_fields):
+        return jsonify({'message': 'Dati mancanti'}), 400
+
+    rows_updated = updatefilm(Id, data)
+    if rows_updated == 1:
+        return jsonify({'message': 'Mammifero aggiornato con successo'}), 200
+    else:
+        return jsonify({'message': 'Errore durante l aggiornamento o ID non trovato'}), 404
+
 
 
 if __name__ == "__main__":
